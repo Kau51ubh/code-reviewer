3bc7ee95a5f83f51b39c23e39af893bdb159b553
kaustubh thakre <86138344+Kau51ubh@users.noreply.github.com>
2026-06-02 23:14:38 +0530

    Delete test_heavy_joins.sql

--- test_heavy_joins.sql
+++
@@ -1,9 +0,0 @@
--- Filename: test_heavy_joins.sql
--- Description: Intentionally messy SQL to trigger architectural AI checks
-
-SELECT * FROM orders o, customers c
-WHERE o.customer_id = c.id
-JOIN inventory i ON o.item_id = i.item_id
-JOIN shipping_status s ON o.order_id = s.order_id
-WHERE o.order_date >= '2026-01-01'
-GROUP BY c.region_id;
