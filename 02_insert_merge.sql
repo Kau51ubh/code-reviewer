423b0aff97e33fdb607f818a98191f148e192082
kaustubh thakre <86138344+Kau51ubh@users.noreply.github.com>
2026-06-02 23:15:11 +0530

    Create 02_insert_merge.sql

---
+++ 02_insert_merge.sql
@@ -0,0 +1,5 @@
+INSERT INTO DB_AEDWD2.customers (customer_id, customer_name, region, created_at, etl_batch_sk) 
+VALUES (101, 'Alice', 'NA', TIMESTAMP('2023-01-01'), 9999);
+
+INSERT INTO DB_AEDWD2.customers (customer_id, customer_name, region, created_at, etl_batch_sk) 
+VALUES (102, 'Bob', 'EMEA', TIMESTAMP('2023-01-02'), 9999);
