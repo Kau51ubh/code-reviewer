8c02eac7d991554903741cf4314c1833801d0ca1
kaustubh thakre <86138344+Kau51ubh@users.noreply.github.com>
2026-06-05 19:14:28 +0530

    Auto-refactor: Optimized 04_cross_join_flawed.sql via AI Architect

--- 04_cross_join_flawed.sql
+++ 04_cross_join_flawed.sql
@@ -1,2 +1,9 @@
-SELECT * FROM DB_AEDWD2.orders o
-JOIN DB_AEDWD2.customers c;
+SELECT
+    o.*,
+    c.*
+FROM
+    ${AEDW_DB}.orders AS o
+INNER JOIN
+    ${AEDW_DB}.customers AS c
+ON
+    o.customer_id = c.customer_id;
