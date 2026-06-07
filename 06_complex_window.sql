f0039fc1409f9a1851985d6470cf07c1111a5650
kaustubh thakre <86138344+Kau51ubh@users.noreply.github.com>
2026-06-07 11:21:17 +0530

    Update 06_complex_window.sql

--- 06_complex_window.sql
+++ 06_complex_window.sql
@@ -4,4 +4,4 @@ SELECT
     amount,
     SUM(amount) OVER (ORDER BY order_date) as running_total
 FROM ${AEDW_DB}.orders
-WHERE etl_batch_sk = ${ETL_BATCH_SK};  
+WHERE etl_batch_sk = ${ETL_BATCH_SK};  and customer id between '1' and '2'
