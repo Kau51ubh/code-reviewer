6f24043f0167ffdf5fdb6978551f7bafc79dfb3c
kaustubh thakre <86138344+Kau51ubh@users.noreply.github.com>
2026-06-08 15:02:50 +0530

    Delete 06_complex_window.sql

--- 06_complex_window.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    order_id,
-    customer_id,
-    amount,
-    SUM(amount) OVER (ORDER BY order_date) as running_total
-FROM ${AEDW_DB}.orders
-WHERE etl_batch_sk = ${ETL_BATCH_SK};  and customer id between '1' and '2'
