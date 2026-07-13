194c1dc675780963ff0200ee0f99587af724d56d
kaustubh thakre <86138344+Kau51ubh@users.noreply.github.com>
2026-07-13 18:25:33 +0530

    Optimized 01_SRCMC11111_INTERFACE.py via AI Code Reviewer

--- 01_SRCMC11111_INTERFACE.py
+++ 01_SRCMC11111_INTERFACE.py
@@ -1,11 +1,11 @@
 from airflow import DAG
 from airflow.configuration import conf
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow.models import Variable
 from airflow_common.operators.DART_SSH_Operator_ETL_VM import dart_custom_ssh_task
 
 #-----------------------Global parameters-------------------------------------
-DAG_NAME = "SRCMC11111_INTERFACE"
+DAG_NAME = "SRCMC11111_INTERFACE_VM"
 COMPOSER_HOME = "/home/airflow/gcs/"
 SQL_DIR = COMPOSER_HOME + "dags/"
 GLOBAL_CONF_DIR = COMPOSER_HOME + "dags/ihg-dart-edw-common-utils/config/"
@@ -16,19 +16,22 @@ default_args = {
     'email': Variable.get('smtp_mail_to'),
     'start_date': datetime(2021, 1, 24),
     'retries': 0,
-	'catchup': True,
+
     'retry_delay': timedelta(minutes=0),
 }
 
 #-----------------------Dag Definition----------------------------------------
-dag = DAG(dag_id=DAG_NAME,
+dag = DAG(
+        catchup=False,
+        max_active_runs=1,
+        tags=["interface"],dag_id=DAG_NAME,
           description='SRCMC11111_INTERFACE',
           schedule_interval=None,
           default_args=default_args,
           template_searchpath=[SQL_DIR],
           )
 
-		  SRCMC10435_1 = dart_custom_ssh_task(
+SRCMC11111_1 = dart_custom_ssh_task(
     task_id="SRCMC11111",
     script_path="""
 ksh -x /home/{{ var.value.GCP_TL_VM_ENV }}/SA/FI/script/SRCMC11111_exec_interface.ksh SRCMC10435 N
@@ -36,4 +39,4 @@ ksh -x /home/{{ var.value.GCP_TL_VM_ENV }}/SA/FI/script/SRCMC11111_exec_interfac
     dag=dag
 )
 
-SRCMC10435_1
+SRCMC11111_1
